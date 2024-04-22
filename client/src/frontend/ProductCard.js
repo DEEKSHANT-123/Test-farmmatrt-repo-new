40336ec28f42c7399117d21b22b90f7e19d71812
pushpendra021 <pushpendrabais021@gmail.com>
2024-04-22 14:28:11 +0530

    add buttons

--- client/src/frontend/ProductCard.js
+++ client/src/frontend/ProductCard.js
@@ -9,6 +9,10 @@ const ProductCard = ({ product }) => {
         {/* <p>{product.Description}</p> */}
         <p>Rs: {product.Rs}</p>
         {/* <p>Warranty: {product.Warranty}</p> */}
+        <div style={{ marginTop: '10px' }}>
+          <button style={{ backgroundColor: 'yellow', marginRight: '10px' }}>Show Details</button>
+          <button style={{ backgroundColor: 'skyblue' }}>Add to Cart</button>
+        </div>
       </div>
     </div>
   );
