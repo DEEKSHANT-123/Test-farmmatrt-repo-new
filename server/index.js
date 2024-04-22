46aab3b8f2cbd0c8d15432fc3db14e073619ba8c
Pushpendra Kumar Bais <61676326+pushpendra021@users.noreply.github.com>
2024-04-22 22:13:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -141,7 +141,7 @@ app.delete("/deleteproduct/:id",async(req,res)=>{
 
 
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
 });
